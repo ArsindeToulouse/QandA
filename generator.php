f95e12077676d44779c04d50558c999a5f606056
arsinde <yulia.rgva@gmail.com>
2016-12-18 19:20:55 +0300

    Deleted generator.php

--- generator.php
+++
@@ -1,7 +0,0 @@
-<?php
-$options = [
-    'cost' => 15,
-];
-//echo password_hash("admin", PASSWORD_BCRYPT, $options)."\n";
-//echo password_hash("moder", PASSWORD_BCRYPT, $options)."\n";
-//echo password_hash("coord", PASSWORD_BCRYPT, $options)."\n";
